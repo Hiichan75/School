4e94ba09a99210434039291b928ec5b7dd4dd75e
Hiichan75 <kimsojung19950712@gmail.com>
2023-10-05 16:04:32 +0200

    new codes 05/10

---
+++ courseOktober/src/loops4_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class loops4_1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("geef ons een tekst");
+        String tekst = sc.nextLine();
+        String klinkers = "aeiouyAEIOUY";
+        char kar;
+        int pos, aantal=0;
+
+        for(int i = 0; i < tekst.length(); i++){
+
+            kar = tekst.charAt(i);
+            pos = klinkers.indexOf(kar);
+            if(pos >= 0){
+                aantal++;
+            }
+            System.out.println(aantal);
+        }
+    }
+}
---
+++ courseOktober/src/loops5.java
@@ -0,0 +1,46 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class loops5 {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int jaartalVerschil = 0;
+
+        int dagen = 0;
+
+        int schrikkeldagen = 0;
+
+        System.out.println("geef uw eerste jaartal (eerste jaartal moet kleiner zijn dan de tweede)");
+
+        int jaartal1 = sc.nextInt();
+
+        System.out.println("geef uw tweede jaartal");
+
+        int jaartal2 = sc.nextInt();
+
+        if(jaartal1 > jaartal2){
+            System.out.println("foutive invoer");
+
+        }else{
+            jaartalVerschil = (jaartal2 - jaartal1);
+
+
+            for(int i= 0; i<= jaartalVerschil; i++) {
+                if(jaartalVerschil >= 0){
+                    dagen += 365;
+
+                }
+
+
+
+            }
+            System.out.println("aantal dagen in de periode [" + jaartal1 + "-" + jaartal2 + "] is " + (dagen + schrikkeldagen));
+
+        }
+    }
+}
+
